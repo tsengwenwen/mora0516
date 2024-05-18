dc89cc16a2a7463883b5e1e44a3237345b96719e
tsengwenwen <msfn387908@gmail.com>
2024-05-18 23:20:37 +0800

    Initial Commit

---
+++ mora0516/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mora0516
+//
+//  Created by TsengWen on 2024/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mora0516/mora0516App.swift
@@ -0,0 +1,17 @@
+//
+//  mora0516App.swift
+//  mora0516
+//
+//  Created by TsengWen on 2024/5/18.
+//
+
+import SwiftUI
+
+@main
+struct mora0516App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
